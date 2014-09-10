492768b9ac5d3882097410153d976c0cf9ce9a26
Huang Ian <huangian@dragoons-Mac.local>
2014-09-10 20:51:14 +0800

    Initial Commit

---
+++ uactest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  uactest
+//
+//  Created by Huang Ian on 14/9/10.
+//  Copyright (c) 2014年 Huang Ian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "iosAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([iosAppDelegate class]));
+    }
+}
